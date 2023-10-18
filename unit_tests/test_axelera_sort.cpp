1ccbec2fd661a1bc35560995d812557e2bdc8ed3
Sevki Kocadag <bekirsevki@gmail.com>
2023-10-19 00:55:48 +0300

    test: Add C++ unit test of the library
    Closes #6

---
+++ unit_tests/test_axelera_sort.cpp
@@ -0,0 +1,37 @@
+#include "gtest/gtest.h"
+#include "axelera_sort.h"
+
+// Test fixture for sorting tests
+class AxeleraSortTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        // Set up any resources you need for the tests here.
+    }
+
+    void TearDown() override {
+        // Clean up any resources used by the tests here.
+    }
+};
+
+TEST_F(AxeleraSortTest, QuickSortTest) {
+    std::vector<int> arr = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
+    Sort::quickSort(arr);
+    ASSERT_EQ(arr, std::vector<int>({1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9}));
+}
+
+TEST_F(AxeleraSortTest, MergeSortTest) {
+    std::vector<int> arr = {38, 27, 43, 3, 9, 82, 10};
+    Sort::mergeSort(arr);
+    ASSERT_EQ(arr, std::vector<int>({3, 9, 10, 27, 38, 43, 82}));
+}
+
+TEST_F(AxeleraSortTest, StdSortTest) {
+    std::vector<int> arr = {17, 12, 56, 32, 8, 47};
+    Sort::stdSort(arr);
+    ASSERT_EQ(arr, std::vector<int>({8, 12, 17, 32, 47, 56}));
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
